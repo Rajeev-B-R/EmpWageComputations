b2657380ca45f11e790bf298e9a048582016e42c
Rajeev <rajeevsuchiha@gmail.com>
2020-07-06 11:35:29 +0530

    Added: Daily Emp Wage Computation added

--- empWage.sh
+++ empWage.sh
@@ -1,11 +1,17 @@
 echo "Welcome to Employee Wage Computation"
 
-isPresent=1
+isPresent=1;
+empRatePerHr=20;
+empHrs=8;
+salary=0;
+
 randomCheck=$(( RANDOM%2 ));
-if [ $isPresent -eq $randomCheck ]
+if [ $isPresent -eq $randomCheck ];
 then
 	echo "Employee is present"
 else
 	echo "Not present"
 fi
 
+salary=$(( $empHrs * $empRatePerHr ));
+echo $salary
