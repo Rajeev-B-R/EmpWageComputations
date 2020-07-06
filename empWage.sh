fb33687d28756c0ef0add279b2c892969a82b480
Rajeev <rajeevsuchiha@gmail.com>
2020-07-06 11:13:22 +0530

    Added:Functionality to check employee present/absent added

--- empWage.sh
+++ empWage.sh
@@ -1,1 +1,11 @@
 echo "Welcome to Employee Wage Computation"
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ));
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is present"
+else
+	echo "Not present"
+fi
+
